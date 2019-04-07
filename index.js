0122c043f711c172eea6e0e877d8724fbcaea39f
Adri Silva <adrisilva0002@gmail.com>
2019-04-06 22:23:53 -0300

    feat: add image robot

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const robots = {
 	text: require('./robots/text'),
 	input: require('./robots/input'),
-	state: require('./robots/state')
+	state: require('./robots/state'),
+	image: require('./robots/image')
 };
 
 async function start()
 {
-	robots.input();
-	await robots.text();
+	// robots.input();
+	// await robots.text();
+	await robots.image();
 
 	const content = robots.state.load();
-
 	console.dir(content, {depth: null});
 }
 
