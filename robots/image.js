45f338da77ae8bc914bc4e22887fd00889870060
Adri Silva <adrisilva0002@gmail.com>
2019-04-15 22:19:07 -0300

    download images

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch  = google.customsearch('v1');
 const state = require('./state');
@@ -7,9 +8,11 @@ const googleSearchCredentials = require('../credentials/google-search');
 async function robot() {
     const content = state.load();
 
-    await fetchImagesOfAllSentences(content);
+    // await fetchImagesOfAllSentences(content);
 
-    state.save(content);
+    // state.save(content);
+
+    await downloadAllImages(content);
 
     async function fetchImagesOfAllSentences(content){
         for(const sentence of content.sentences){
@@ -34,6 +37,43 @@ async function robot() {
         });
 
         return imagesUrl;
+    }
+
+    async function downloadAllImages(content)
+    {
+        content.downloadImages = [];
+
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+
+            const images = content.sentences[sentenceIndex].images;
+
+            for(let imageIndex = 0; imageIndex < images.length; imageIndex++){
+
+                const imageUrl = images[imageIndex];
+
+                try{
+                    if(content.downloadImages.includes(imageUrl)){
+                        throw new Error('Imagem já foi baixada!');
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+                    content.downloadImages.push(imageUrl);
+                    console.log(`> Baixou imagem com sucesso ${imageUrl}`);
+                    break;
+                }catch(error){
+                    console.log(`> Erro ao baixar (${imageUrl}): ${error}`);
+
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName)
+    {
+        return imageDownloader.image({
+           url: url,
+           dest: `./content/${fileName}`
+        });
     }
 }
 
